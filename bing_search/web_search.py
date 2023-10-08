399b0f029c2317865dea07c2658da80216a2c8b4
huangxinzhe <315680524@qq.com>
2023-10-08 13:52:12 +0800

    bing web search

--- bing_search/web_search.py
+++ bing_search/web_search.py
@@ -1,21 +1,21 @@
-from config import BING_KEY, BING_NEWS_SEARCH_URL
+from config import BING_KEY, BING_WEB_SEARCH_URL
 import requests
 
-class BingNewsSearch():
+class BingWebSearch():
 
     def __init__(self) -> None:
         pass
 
-    def get_news(self, query):
+    def get_web(self, query):
         headers = {"Ocp-Apim-Subscription-Key" : BING_KEY}
         params  = {"q": query, "textDecorations": True, "textFormat": "HTML"}
-        response = requests.get(BING_NEWS_SEARCH_URL, headers=headers, params=params)
+        response = requests.get(BING_WEB_SEARCH_URL, headers=headers, params=params)
         response.raise_for_status()
         search_results = response.json()
         return search_results
 
 if __name__ == "__main__":
-    bing_news_search = BingNewsSearch()
-    search_results = bing_news_search.get_news("Microsoft")
+    bing_web_search = BingWebSearch()
+    search_results = bing_web_search.get_web("Microsoft")
     print(search_results)
 
